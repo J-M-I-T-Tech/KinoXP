a8c916968968936184039bf11c0714f6528a2af9
Gotze <thorholby@gmail.com>
2026-03-06 11:10:44 +0100

    Add Showing model class

---
+++ src/main/java/com/kinoxp/model/showing/Showing.java
@@ -0,0 +1,62 @@
+package com.kinoxp.model.showing;
+
+import com.kinoxp.model.movie.Movie;
+
+import java.time.LocalDateTime;
+
+public class Showing {
+    private int showingId;
+    private Movie movie;
+    private Theater theater;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public Showing(int showingId, Movie movie, Theater theater,
+                   LocalDateTime startTime, LocalDateTime endTime) {
+        this.showingId = showingId;
+        this.movie = movie;
+        this.theater = theater;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public int getShowingId() {
+        return showingId;
+    }
+
+    public void setShowingId(int showingId) {
+        this.showingId = showingId;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public Theater getTheater() {
+        return theater;
+    }
+
+    public void setTheater(Theater theater) {
+        this.theater = theater;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+}
