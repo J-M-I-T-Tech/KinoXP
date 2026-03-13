632e518d810c5d8f05ef8be4674851bdde90b9ba
moha4733 <moonyahidi@gmail.com>
2026-03-13 13:14:45 +0100

    reservation knap for en kunde kan se deres reservationer, kan redigere og anullere

--- src/main/java/com/kinoxp/dto/ReservationResponse.java
+++ src/main/java/com/kinoxp/dto/ReservationResponse.java
@@ -9,12 +9,16 @@ import java.util.List;
 public record ReservationResponse(
         Long reservationId,
         Long showingId,
+        Long movieId,
         String movieTitle,
         String customerName,
         List<Long> seatIds,
         double totalPrice,
         BookingStatus bookingStatus,
         PaymentStatus paymentStatus,
-        LocalDateTime createdAt
+        LocalDateTime createdAt,
+        LocalDateTime showingStartTime,
+        String theaterName
+
 ) {
 }
